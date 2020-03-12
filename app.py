c4ebb5b9ab84a704c9e95ac969e99e3dd0d84e1b
Shawn Peng <shawn.peng@autodesk.com>
2020-03-12 17:21:07 +0800

    Renamed the html file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def fileFrontPage():
-    return render_template('fileform.html')
+    return render_template('index.html')
 
 @app.route("/handleUpload", methods=['POST'])
 def handleFileUpload():
